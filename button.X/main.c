56e7f51df03fc97e9b0ffd59e5feb1c119329a9a
Devilbinder <Devilbinder@users.noreply.github.com>
2018-09-18 22:12:20 +0200

    first

---
+++ button.X/main.c
@@ -0,0 +1,30 @@
+#include <xc.h>
+#include <p18f4520.h>
+#include "conbits.h"
+
+void main(void) {
+    TRISB=0; //0b00000000
+    TRISDbits.RD7=1; //0b10000000
+    TRISDbits.RD6=1; //0b11000000
+    
+    LATB=0; //0b00000000
+   
+    while(1){
+        
+        if(PORTDbits.RD7==1){ //0b10000000
+            
+			LATBbits.LB0=1;
+            
+        }else{
+			LATBbits.LB0=0;
+		}
+		
+		if(PORTDbits.RD6==1){ //0b10000000
+            
+			LATBbits.LB1=1;
+            
+        }else{
+			LATBbits.LB1=0;
+		}
+    }    
+}
